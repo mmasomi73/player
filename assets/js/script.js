aadf8fba25e005d68c2eea24de5d0010e0b562e1
mma <mmasomi73@gmail.com>
2019-05-25 00:26:03 +0430

    #8
    readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,7 +174,6 @@ $(document).ready(function() {
         $('.prog').css('width',((clkX/relX)*100)+'%');
         wavesurfer.seekTo((clkX/relX));
     });
-
-    var song = new Audio();
+    
 
 });
