c3cc9263cc5bec615a6112570268703baf34ddd4
mma <mmasomi73@gmail.com>
2019-05-28 10:21:21 +0430

    #13
    Responsivity

---
+++ assets/js/mobile.js
@@ -0,0 +1,26 @@
+
+function resp() {
+    var width = $(window).width();
+    //-----= Responsive
+        //-----= Audio volume
+        // console.log();
+        // $('.vlCtrl svg').attr('viewBox','0 0 '+(width - (width * 0.1) - 120)+' 30');
+        // $('.vlCtrl svg #ctrlLineB').attr('x2',(width - (width * 0.1) - 128)).attr('y2',15).attr('y1',15);
+        // $('.vlCtrl svg #ctrlLineF').attr('x2',(width - (width * 0.1) - 128)).attr('y2',15).attr('y1',15);
+        // $('.vlCtrl svg #circle').attr('cx',(width - (width * 0.1) - 128)).attr('cy',15);
+    if (width <= 500){
+
+    }
+}
+
+$(window).ready(function() {
+    resp();
+    $('.plist').click(function () {
+        $('.playlist').toggleClass('shower');
+        $('.player').toggleClass('hider');
+    });
+    $('.playlist  .repeat').click(function () {
+        $('.playlist').toggleClass('shower');
+        $('.player').toggleClass('hider');
+    });
+});
